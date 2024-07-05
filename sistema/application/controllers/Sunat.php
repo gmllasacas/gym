a39d4165a6fb8f24c3f53a76236c9576c9c4f717
gmllasacas <gmllasacas@gmail.com>
2024-07-05 11:34:42 -0500

    #8, Agregado la conexion con sunat e impresion en html

---
+++ sistema/application/controllers/Sunat.php
@@ -0,0 +1,168 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+//require FCPATH . 'vendor/autoload.php';
+
+use Greenter\Ws\Services\SunatEndpoints;
+use Greenter\See;
+use Greenter\Model\Client\Client;
+use Greenter\Model\Company\Company;
+use Greenter\Model\Company\Address;
+use Greenter\Model\Sale\FormaPagos\FormaPagoContado;
+use Greenter\Model\Sale\Invoice;
+use Greenter\Model\Sale\SaleDetail;
+use Greenter\Model\Sale\Legend;
+use Greenter\Report\HtmlReport;
+use Greenter\Report\PdfReport;
+use Greenter\Report\Resolver\DefaultTemplateResolver;
+
+class Sunat extends CI_Controller
+{
+    public $configuracion = [];
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->configuracion = basedetalleregistro('base_configuracion', ['id' => 1]);
+        $this->configuracion['logo'] = ($this->configuracion['logo'] == '') ? 'public/img/recursos/logo.png' : $this->configuracion['logo'];
+        $this->load->helper('text');
+        $this->load->model('GenericoModelo', 'generico_modelo');
+    }
+
+    public function factura()
+    {
+        $see = new See();
+        $see->setCertificate(file_get_contents(APPPATH.'third_party/certificate.pem'));
+        $see->setService(SunatEndpoints::FE_BETA);
+        $see->setClaveSOL('20000000001', 'MODDATOS', 'moddatos');
+
+        // Cliente
+        $client = (new Client())
+            ->setTipoDoc('6')
+            ->setNumDoc('20000000001')
+            ->setRznSocial('EMPRESA X');
+
+        // Emisor
+        $address = (new Address())
+            ->setUbigueo('150101')
+            ->setDepartamento('LIMA')
+            ->setProvincia('LIMA')
+            ->setDistrito('LIMA')
+            ->setUrbanizacion('-')
+            ->setDireccion('Av. Villa Nueva 221')
+            ->setCodLocal('0000'); // Codigo de establecimiento asignado por SUNAT, 0000 por defecto.
+
+        $company = (new Company())
+            ->setRuc('20123456789')
+            ->setRazonSocial('GREEN SAC')
+            ->setNombreComercial('GREEN')
+            ->setAddress($address);
+
+        // Venta
+        $invoice = (new Invoice())
+            ->setUblVersion('2.1')
+            ->setTipoOperacion('0101') // Venta - Catalog. 51
+            ->setTipoDoc('01') // Factura - Catalog. 01
+            ->setSerie('F001')
+            ->setCorrelativo('2')
+            ->setFechaEmision(new DateTime('2020-08-24 13:05:00-05:00')) // Zona horaria: Lima
+            ->setFormaPago(new FormaPagoContado()) // FormaPago: Contado
+            ->setTipoMoneda('PEN') // Sol - Catalog. 02
+            ->setCompany($company)
+            ->setClient($client)
+            ->setMtoOperGravadas(100.00)
+            ->setMtoIGV(18.00)
+            ->setTotalImpuestos(18.00)
+            ->setValorVenta(100.00)
+            ->setSubTotal(118.00)
+            ->setMtoImpVenta(118.00)
+            ;
+
+        $item = (new SaleDetail())
+            ->setCodProducto('P001')
+            ->setUnidad('NIU') // Unidad - Catalog. 03
+            ->setCantidad(2)
+            ->setMtoValorUnitario(50.00)
+            ->setDescripcion('PRODUCTO 1')
+            ->setMtoBaseIgv(100)
+            ->setPorcentajeIgv(18.00) // 18%
+            ->setIgv(18.00)
+            ->setTipAfeIgv('10') // Gravado Op. Onerosa - Catalog. 07
+            ->setTotalImpuestos(18.00) // Suma de impuestos en el detalle
+            ->setMtoValorVenta(100.00)
+            ->setMtoPrecioUnitario(59.00)
+            ;
+
+        $legend = (new Legend())
+            ->setCode('1000') // Monto en letras - Catalog. 52
+            ->setValue('SON DOSCIENTOS TREINTA Y SEIS CON 00/100 SOLES');
+
+        $invoice->setDetails([$item])
+                ->setLegends([$legend]);
+        
+        $result = $see->send($invoice);
+
+        // Guardar XML firmado digitalmente.
+        file_put_contents(
+            FCPATH . '/public/documentos/sunat/' . $invoice->getName().'.xml',
+            $see->getFactory()->getLastXml()
+        );
+
+        // Verificamos que la conexión con SUNAT fue exitosa.
+        if (!$result->isSuccess()) {
+            // Mostrar error al conectarse a SUNAT.
+            echo 'Codigo Error: '.$result->getError()->getCode();
+            echo 'Mensaje Error: '.$result->getError()->getMessage();
+            exit();
+        }
+
+        // Guardamos el CDR
+        file_put_contents(FCPATH . '/public/documentos/sunat/' . 'R-'.$invoice->getName().'.zip', $result->getCdrZip());
+
+        $cdr = $result->getCdrResponse();
+
+        $code = (int)$cdr->getCode();
+
+        if ($code === 0) {
+            //echo 'ESTADO: ACEPTADA'.PHP_EOL;
+
+            $report = new HtmlReport();
+            $report->setTemplate('invoice3.html.twig');
+
+            $params = [
+                'system' => [
+                    'logo' => file_get_contents(FCPATH.'/public/img/recursos/logo.png'),
+                    'hash' => 'qqnr2dN4p/HmaEA/CJuVGo7dv5g=',
+                ],
+                'user' => [
+                    'header'     => 'Telf: <b>(01) 123375</b>', // Texto que se ubica debajo de la dirección de empresa
+                    'extras'     => [
+                        ['name' => 'CONDICION DE PAGO', 'value' => 'Efectivo'     ],
+                        ['name' => 'VENDEDOR'         , 'value' => 'GITHUB SELLER'],
+                    ],
+                    'footer' => '<p>Nro Resolucion: <b>3232323</b></p>'
+                ]
+            ];
+
+            $html = $report->render($invoice, $params);
+
+            echo $html;
+
+            //file_put_contents('invoice.pdf', $pdf);
+
+            if (count($cdr->getNotes()) > 0) {
+                echo 'OBSERVACIONES:'.PHP_EOL;
+                // Corregir estas observaciones en siguientes emisiones.
+                var_dump($cdr->getNotes());
+            }
+        } else if ($code >= 2000 && $code <= 3999) {
+            echo 'ESTADO: RECHAZADA'.PHP_EOL;
+        } else {
+            /* Esto no debería darse, pero si ocurre, es un CDR inválido que debería tratarse como un error-excepción. */
+            /*code: 0100 a 1999 */
+            echo 'Excepción';
+        }
+
+        echo $cdr->getDescription().PHP_EOL;
+    }
+}
